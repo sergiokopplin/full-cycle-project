511441c92eedda3c7d1d389fe4e5e7494536eb68
Sergio Kopplin <sergiokopplin@gmail.com>
2022-07-17 17:03:26 -0300

    test: ensure entities created_at runs on constructor

--- src/category/tests/unit/domain/test_entities.py
+++ src/category/tests/unit/domain/test_entities.py
@@ -9,10 +9,26 @@ class TestCategory(unittest.TestCase):
     def test_if_is_a_dataclass(self):
         self.assertEqual(is_dataclass(Category), True)
 
-    def test_constructor(self):
-        category = Category('Movie', 'Some Description', True, datetime.now())
+    def test_constructor_with_optionals(self):
+        category = Category('Movie')
+
+        self.assertEqual(category.name, 'Movie')
+        self.assertEqual(category.description, None)
+        self.assertEqual(category.is_active, True)
+        self.assertIsInstance(category.created_at, datetime)
+
+
+    def test_constructor_without_optionals(self):
+        created_at = datetime.now()
+        category = Category('Movie', 'Some Description', False, created_at)
 
         self.assertEqual(category.name, 'Movie')
         self.assertEqual(category.description, 'Some Description')
-        self.assertEqual(category.is_active, True)
+        self.assertEqual(category.is_active, False)
         self.assertIsInstance(category.created_at, datetime)
+
+    def test_if_created_at_is_generated_in_constructor(self):
+        category1 = Category('Movie 1')
+        category2 = Category('Movie 2')
+
+        self.assertNotEqual(category1.created_at.timestamp(), category2.created_at.timestamp())
