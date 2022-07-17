bbbfc32f24883caa6efd429690d3cdf4869ce639
Sergio Kopplin <sergiokopplin@gmail.com>
2022-07-17 16:47:26 -0300

    feat: implements dataclasses on category entities

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,10 +1,11 @@
 from datetime import datetime
+from dataclasses import dataclass, field
+from typing import Optional
 
-
+@dataclass()
 class Category:
-    
-    def __init__(self, name: str, description: str, is_active: bool, created_at: datetime) -> None:
-        self.name = name
-        self.description = description
-        self.is_active = is_active
-        self.created_at = created_at
+
+    name: str
+    description: Optional[str] = None
+    is_active: Optional[bool] = True
+    created_at: Optional[datetime] = field(default_factory=lambda: datetime.now())
