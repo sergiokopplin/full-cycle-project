715f47cb15967278a6e4f818cd697f604cc499db
Sergio Kopplin <sergiokopplin@gmail.com>
2022-07-21 23:36:32 -0300

    feat: adds uuid on entities domain

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,11 +1,14 @@
 from datetime import datetime
 from dataclasses import dataclass, field
 from typing import Optional
+import uuid
 
-@dataclass()
+@dataclass(kw_only=True)
 class Category:
 
+    id: uuid.UUID = field(default_factory=lambda: uuid.uuid4())
     name: str
     description: Optional[str] = None
     is_active: Optional[bool] = True
     created_at: Optional[datetime] = field(default_factory=lambda: datetime.now())
+ 
